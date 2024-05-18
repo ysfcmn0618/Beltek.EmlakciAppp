bdac67bcec8b36fc26f1a4559bf7933ea73145fd
yusuf çimen <yusufcimen90@windowslive.com>
2024-05-18 10:58:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Beltek.EmlakciApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
